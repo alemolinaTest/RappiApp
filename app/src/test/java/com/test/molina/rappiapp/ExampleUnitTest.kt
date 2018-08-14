e9ecbd83c88cd7032b42ce2a7267e7ea507636a0
alemolinaTest <41317511+alemolinaTest@users.noreply.github.com>
2018-08-14 20:16:14 -0300

    unit test
    som testings

--- app/src/test/java/com/test/molina/rappiapp/ExampleUnitTest.kt
+++ app/src/test/java/com/test/molina/rappiapp/ExampleUnitTest.kt
@@ -1,7 +1,10 @@
 package com.test.molina.rappiapp
 
+import com.test.molina.rappiapp.data.remote.AppApiHelper
+import com.test.molina.rappiapp.utils.CommonUtils
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.Mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +12,29 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun capital_ShouldIncorrect() {
+        assertEquals(true, CommonUtils.checkCapital("m"))
     }
+
+    @Test
+    fun capital_ShouldCorrect() {
+        assertEquals(true, CommonUtils.checkCapital("M"))
+    }
+
+    @Test
+    fun capital_TryNull() {
+        assertEquals(false, CommonUtils.checkCapital(null))
+    }
+
+    @Test
+    fun valid_Email() {
+        assertEquals(true, CommonUtils.isEmailValid("ale@gmail.com"))
+        assertEquals(false, CommonUtils.isEmailValid("ale@gmail"))
+        assertEquals(true, CommonUtils.isEmailValid("ale@gmail"))
+    }
+   
+
 }
